3f297a3cf46a92dc60c22c1ecfe3bbd3ed134a9b
thinhcubi <thinhcubi27@gmail.com>
2021-12-11 16:53:24 +0700

    database

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-
-
+    public function user(){
+        return $this->belongsTo('App\User','user_id');
+    }
+    public function category(){
+        return $this->belongsTo('App\Category','category_id');
+    }
 }
