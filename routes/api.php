32bd23aef6a09590a34f6d18530c1a428da686d9
nlam0608 <nlam06082001@gmail.com>
2021-12-14 10:54:21 +0700

    login_backend2

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::post('/register', 'App\Http\Controllers\UserController@register');
 
-Route::post('login', 'App\Http\Controllers\UserController@authenticate');
+Route::post('/login', 'App\Http\Controllers\UserController@authenticate');
 
 Route::group(['/middleware' => ['jwt.verify']], function() {
 
