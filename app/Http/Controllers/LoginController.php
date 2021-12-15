9d702368f93b610c994ff0e6ce1e4879c9d01eca
nlam0608 <nlam06082001@gmail.com>
2021-12-15 09:21:38 +0700

    xoa cac khoang trang

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,21 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\User;
-
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Hash;
-
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
-
-
-
 use Tymon\JWTAuth\Exceptions\JWTException;
-
-
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class LoginController extends Controller
